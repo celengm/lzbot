4d062469ab24fbba5a5e4344ad182b7c2c687de1
Howard Weng <howard@soslabde-MacBook-Pro.local>
2018-05-16 18:20:53 +0800

    deltet cmd box

--- app.py
+++ app.py
@@ -131,33 +131,38 @@ def slient_mode(user_message,event):
 		message = TextSendMessage(text='我已經閉嘴了 > <  (小聲)')
 		line_bot_api.reply_message(event.reply_token,message)
 
-def search_cmd(user_message):
-	operations_str = [
-	[["!閉嘴","!安靜","!你閉嘴","!你安靜"],switch_off()],
-	[["!說話"],switch_still_on()],
-	[["!使用說明書","!help","!說明書"],readme()],
-	[["即時排名","即時戰況",'排名','分數','戰況','score'],leaderboard(2)],
-	[["%數","%"],leaderboard(3)],
-	[['分數差'],leaderboard(5)],
-	[['場數差'],leaderboard(6)], 
-	[["追擊時間","脫褲子","脫內褲","內褲","褲子"],leaderboard(7)],
-	[['時速'],leaderboard(8)], 
-	[['場速'],leaderboard(9)],
-	[["活動進度",'進度'],event_progress()]
-	]
-
-	for i in range(len(operations_str)):
-		if user_message in operations_str[i][0]:
-			return TextSendMessage(text= operations_str[i][1])
-
-	return "not found in cmd list"
-
 def active_mode(user_message,event):
 	global mode
-	message_get = search_cmd(user_message.lower())
-
-	if str(message_get) != "not found in cmd list" :
-		line_bot_api.reply_message(event.reply_token,message_get)
+	print ("start seraching key in cmd box ...")
+	message = "default"
+	if(user_message in ["!閉嘴","!安靜","!你閉嘴","!你安靜"]):
+		message = switch_off()
+	elif(user_message in ["!說話"]):
+		message = switch_still_on()
+	elif(user_message in ["!使用說明書","!help","!說明書"]):
+		message = readme()
+	elif(user_message in ["即時排名","即時戰況",'排名','分數','戰況','score']):
+		message = leaderboard(2)
+	elif(user_message in ["%數","%"]):
+		message = leaderboard(3)
+	elif(user_message in ['分數差']):
+		message = leaderboard(5)
+	elif(user_message in ['場數差']):
+		message = leaderboard(6)
+	elif(user_message in ["追擊時間","脫褲子","脫內褲","內褲","褲子"]):
+		message = leaderboard(7)
+	elif(user_message in ['時速']):
+		message = leaderboard(8)
+	elif(user_message in ['場速']):
+		message = leaderboard(9)
+	elif(user_message in ["活動進度",'進度']):
+		message = event_progress()
+	
+	if message != "default" :
+		line_bot_api.reply_message(event.reply_token,TextSendMessage(text=message))
+		return
+	print ("key not found in cmd box !")
+	
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
