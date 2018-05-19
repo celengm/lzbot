6330fc5238dc1e1c4e8f9a936da6297d7d9ac84d
Howard Weng <howard@soslabde-MacBook-Pro.local>
2018-05-20 01:28:32 +0800

    fire and stone calculator

--- app.py
+++ app.py
@@ -57,6 +57,35 @@ now = datetime.datetime.now()
 today = time.strftime("%c")
 mode = 1
 
+def fire_calculator(user_message):
+	try:
+		split = user_message.split(" ",2)
+		fire = int(split[1])
+		percent = int(split[2])
+	except:
+		return "【請依照範例輸入：】\n!fire (剩餘火量) (活動％,不用打％)"
+
+	message = "目前所剩火量:"+str(fire)+"\n可轉換分數:"+str(int(fire*3900/3*(1+(percent/100))))
+	return message
+
+def stone_calculator(user_message):
+	global score_sheet_ID
+	try:
+		split = user_message.split(" ",2)
+		stone = int(split[1])
+		percent = int(split[2])
+	except:
+		return "【請依照範例輸入：】\n!stone (剩餘石頭) (活動％,不用打％)"
+
+	values = get_value_from_google_sheet(score_sheet_ID,'E14')
+	if not values:
+		print('No data found.')
+	else:
+		for row in values:	
+			time = 126-round(float(row[0]),2)
+	message = "活動剩餘時間:"+str(round(time,2))+"\n目前所剩石頭量:"+str(stone)+"\n全速到結束所需石頭:"+str(int(time*25*3*100/10))+"\n仍缺少石頭:"+str(int(time*25*3*100/10)-stone)+"\n剩餘石頭可轉換分數:"+str(int(stone/100*10/3*3900*(1+((percent)/100))))
+	return message
+
 def get_value_from_google_sheet(SPREADSHEET_ID,RANGE_NAME):
 	# Setup the Sheets API
 	SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
@@ -256,6 +285,10 @@ def active_mode(user_message,event):
 		message = room_update(user_message)
 	elif(user_message.find("r2") == 0):
 		message = room_update2(user_message)
+	elif(user_message.find("!fire") == 0):
+		message = fire_calculator(user_message)
+	elif(user_message.find("!stone") == 0):
+		message = stone_calculator(user_message)
 	
 	if message != "default" :
 		line_bot_api.reply_message(event.reply_token,TextSendMessage(text=message))
